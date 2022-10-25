6327c15b212db0c5707897776c8330ebaea76b71
Victor Fernandez Flores <victofer@student.42.fr>
2022-10-25 14:07:22 +0200

    Feature: Add various

---
+++ various/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 10:55:26 by victofer          #+#    #+#             */
+/*   Updated: 2022/10/25 11:55:37 by victofer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *str);
+char	*get_line_gnl(char *str);
+char	*save_rest(char *str);
+char	*ft_join_free(char *buffer, char *buff);
+
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+void	*ft_calloc(size_t count, size_t size);
+#endif
