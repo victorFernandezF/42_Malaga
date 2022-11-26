e872963b0f43cb828396d0ccf43aaef225ac5ab9
Victor Fernandez Flores <victofer@student.42.fr>
2022-11-26 10:50:39 +0100

    Refactor: Changed exam practice files

--- various/exam_practice/list.h
+++ various/exam_practice/list.h
@@ -6,7 +6,18 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 18:33:54 by victofer          #+#    #+#             */
-/*   Updated: 2022/11/25 18:35:25 by victofer         ###   ########.fr       */
+/*   Updated: 2022/11/26 10:42:57 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIST_H
+# define LIST_H
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	int				data;
+	struct s_list	*next;
+}					t_list;
+#endif
--- various/exam_practice/sort_list.c
+++ various/exam_practice/sort_list.c
@@ -6,14 +6,30 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 18:35:28 by victofer          #+#    #+#             */
-/*   Updated: 2022/11/25 18:37:05 by victofer         ###   ########.fr       */
+/*   Updated: 2022/11/26 10:48:26 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"list.h"
-#include<unistd.h>
 
 t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-	
+	int		aux;
+	t_list	*tmp;
+
+	tmp = lst;
+	while (lst->next != NULL)
+	{
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
+		{
+			aux = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = aux;
+			lst = tmp;
+		}
+		else
+		lst = lst->next;
+	}
+	lst = tmp;
+	return (lst);
 }
